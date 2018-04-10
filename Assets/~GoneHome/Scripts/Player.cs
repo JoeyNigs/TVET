69cf44966e10205927670ff70e9ea571e834ab2f
JoeyNigs <Scott.Niguidula@education.nsw.gov.au>
2018-04-10 18:01:24 +1000

    Started Programming Player script ; )

--- Assets/~GoneHome/Scripts/Player.cs
+++ Assets/~GoneHome/Scripts/Player.cs
@@ -1,10 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+/*
+ [] - Brackets
+ {} - Braces
+ () - Parenthsis
 
+Hot Keys:
+ - Clear: CTRL + K + D
+ - Fold: CTRL + M + O
+ - Unfold: CTRL + M + P
+ */
 public class Player : MonoBehaviour
 {
+    public string message = "Sunset Shimmer";
+    public Rigidbody rigid;
 
+    float speed = 5;
+   
     // Use this for initialization
     void Start()
     {
@@ -14,6 +27,9 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKey(KeyCode.K))
+        {
+            rigid.AddForce(Vector3.forward * speed);
+        }
     }
 }
