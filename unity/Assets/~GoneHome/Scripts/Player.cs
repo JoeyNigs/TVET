d0ec949d296196f6e7fac019ad7bd08f486ea67d
JoeyNigs <Scott.Niguidula@education.nsw.gov.au>
2018-05-01 17:51:20 +1000

    Added "Hello World" to text file

---
+++ unity/Assets/~GoneHome/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ [] - Brackets
+ {} - Braces
+ () - Parenthsis
+
+Hot Keys:
+ - Clear: CTRL + K + D
+ - Fold: CTRL + M + O
+ - Unfold: CTRL + M + P
+ */
+public class Player : MonoBehaviour
+{
+    public string message = "Sunset Shimmer";
+    public Rigidbody rigid;
+
+    float speed = 5;
+   
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.K))
+        {
+            rigid.AddForce(Vector3.forward * speed);
+        }
+    }
+}
