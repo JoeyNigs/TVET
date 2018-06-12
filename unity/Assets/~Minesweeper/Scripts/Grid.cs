5ec84f700be346468e524a5bd2a670a6600a3a7a
JoeyNigs <Scott.Niguidula@education.nsw.gov.au>
2018-06-12 17:53:27 +1000

    continued minesweeper

--- unity/Assets/~Minesweeper/Scripts/Grid.cs
+++ unity/Assets/~Minesweeper/Scripts/Grid.cs
@@ -26,7 +26,7 @@ namespace Minesweeper
             tiles = new Tile[width, height];
             for (int x = 0; x < width; x++)
             {
-                for (int y = 0; y < height; x++)
+                for (int y = 0; y < height; y++)
                 {
                     Vector2 halfSize = new Vector2(width * 0.5f, height * 0.5f);
                     Vector2 pos = new Vector2(x - halfSize.x, y - halfSize.y);
@@ -44,7 +44,48 @@ namespace Minesweeper
         }
         void Start()
         {
+            GenerateTitle();
+        }
+        public int GetAdjacentMineCount(Tile tile)
+        {
+            int count = 0;
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; x <= 1; y++)
+                {
+                    int desiredX = tile.x + x;
+                    int desiredY = tile.y + y;
+                    if (desiredX < 0 || desiredX >= width || desiredY < 0 || desiredY >= height)
+                    {
+                        continue;
+                    }
+                    Tile currentTile = tiles[desiredX, desiredY];
+                    if (currentTile.isMine)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+        void FFuncover(int x, int y, bool[,] visited)
+        {
+            if (x >= 0 && y >= 0 && x < width && y < height)
+            {
+                if (visited[x, y])
+                    return;
+                Tile tile = tiles[ x, y];
+                int adjacentMines = GetAdjacentMineCount(tile);
+                tile.Reveal(adjacentMines);
 
+                if (adjacentMines == 0)
+                {
+                    visited[x, y] = true;
+                    FFuncover(x - 1, y, visited);
+                    FFuncover(x + 1, y, visited);
+                }
+            }
+    
         }
     }
 }
